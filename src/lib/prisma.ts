aac69ef3a3ce83da6b838338445f56d7f52e76fb
HireMeFullTime <2017pracownik@gmail.com>
2026-05-24 18:23:53 +0200

    fix: initialize Prisma client instance with global caching for development mode

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,11 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma || new PrismaClient({
-    datasourceUrl: process.env.PRISMA_DATABASE_URL,
+    datasources: {
+      db: {
+        url: process.env.PRISMA_DATABASE_URL,
+      },
+    },
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
