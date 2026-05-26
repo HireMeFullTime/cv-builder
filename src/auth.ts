534eaa7ca48acb24c77f8ff4840e06926c685c83
HireMeFullTime <2017pracownik@gmail.com>
2026-05-26 10:17:10 +0200

    fix: add trustHost to auth config

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,7 @@ import { prisma } from "./lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
   providers: [
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID,
